fbb5a06e23e0ffa1ae7b4b6aa65cb96792884db0
altin <kelmendialtin48@gmail.com>
2022-06-17 19:32:04 +0200

    Controller fertiggestellt!

--- src/main/java/com/example/slm_maintenancemonitorv10/controller/MonitorController.java
+++ src/main/java/com/example/slm_maintenancemonitorv10/controller/MonitorController.java
@@ -1,8 +1,6 @@
 package com.example.slm_maintenancemonitorv10.controller;
 
-
 import org.springframework.web.bind.annotation.*;
-import javax.ws.rs.core.Response;
 
 @CrossOrigin
 @RestController
@@ -10,11 +8,6 @@ import javax.ws.rs.core.Response;
 public class MonitorController {
     private static String status = "-";
 
-    /*@GetMapping()
-    public Response getStatus(){
-        Response.ResponseBuilder rb = Response.ok(status);
-        return rb.header("Access-Control-Allow-Origin", "*").build();
-    }*/
     @GetMapping()
     public String getStatus(){
         return status;
@@ -24,7 +17,6 @@ public class MonitorController {
     public String setStatus(@PathVariable("status") String s){
         if (s == null || s.isEmpty()){
             return status;
-
         } else{
             status = s;
             return status;
